33c1bdc510c6d88b895ffc0548fa26dd097c139d
Levi Donaldson <lcdbacanas@gmail.com>
2015-07-28 17:42:52 -0700

    json placeholder added

---
+++ server.js
@@ -0,0 +1,13 @@
+$(function () {
+        
+
+    var root = 'http://jsonplaceholder.typicode.com';
+
+    $.ajax({
+      url: root + '/posts/1',
+      method: 'GET'
+    }).then(function(data) {
+      console.log(data);
+    });
+
+})
